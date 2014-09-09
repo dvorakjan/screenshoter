d8463e80f35a9d56dfe33480f38f8573dc4e2d7b
root <root@assassin.ebrana.cz>
2014-09-09 13:17:08 +0200

    transformed run.sh into init.d runscript

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-cd ${0%/*}
-mkdir -p logs
-forever stop screenshoter.js > /dev/null 2>&1
-forever start -a -p . --minUptime 1000 --spinSleepTime 100 -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile ./forever.pid -w --watchIgnore "logs/*" screenshoter.js
+#/etc/init.d/screenshoter
+
+export PATH=$PATH:/usr/local/bin
+export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
+
+case "$1" in
+  start)
+  cd $(dirname `readlink -f $0 || realpath $0`)
+  mkdir -p logs
+  forever stop screenshoter.js > /dev/null 2>&1
+  forever start -a -p . --minUptime 1000 --spinSleepTime 100 -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile ./forever.pid -w --watchIgnore "logs/*" screenshoter.js
+  ;;
+stop)
+  exec forever stop screenshoter.js
+  ;;
+*)
+  echo "Usage: /etc/init.d/screenshoter {start|stop}"
+  exit 1
+  ;;
+esac
+
+exit 0
+
